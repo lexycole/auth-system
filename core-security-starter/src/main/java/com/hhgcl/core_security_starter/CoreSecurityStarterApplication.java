6bd44105bfccfd5b85a3a0833709d2b595d7d79e
lexycole <akinbolawa90@gmail.com>
2026-04-08 06:20:30 +0100

    feat: complete installation and setup

---
+++ core-security-starter/src/main/java/com/hhgcl/core_security_starter/CoreSecurityStarterApplication.java
@@ -0,0 +1,13 @@
+package com.hhgcl.core_security_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSecurityStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreSecurityStarterApplication.class, args);
+	}
+
+}
---
+++ core-security-starter/src/test/java/com/hhgcl/core_security_starter/CoreSecurityStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhgcl.core_security_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreSecurityStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
